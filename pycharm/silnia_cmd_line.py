96e7ea63f1fc1d6d402cb021acbcc5b4deb4ee6a
Andrzej Buchowicz <andrzej.buchowicz@gmail.com>
2023-11-16 21:48:01 +0100

    wyklad 6:poprawki/uzupelnienie

---
+++ pycharm/silnia_cmd_line.py
@@ -0,0 +1,19 @@
+import sys
+
+from silnia import silnia_rekur
+
+
+def main():
+    for param in sys.argv:
+        if param.isdigit():
+            n = int(param)
+            print(f"{n}! = {silnia_rekur(n):3d}")
+        else:
+            print(f"{param} nie jest liczba!")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
