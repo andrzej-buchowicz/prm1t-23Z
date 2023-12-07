8fbea50a04bfcbca69a18371d16d0734e95d669e
Andrzej Buchowicz <andrzej.buchowicz@gmail.com>
2023-12-07 20:35:07 +0100

    testy 2

--- pycharm/testy/test_sinus.py
+++ pycharm/testy/test_sinus.py
@@ -1,12 +1,13 @@
 import unittest
+import math
 
 from sinus import sinus
 
 
 class SinusTestCase(unittest.TestCase):
     def test_poprawny_wynik(self):
-        wynik = sinus(0)
-        self.assertEqual(wynik, 0.)
+        self.assertEqual(sinus(0.), 0.)
+        self.assertEqual(sinus(math.pi/2), 1.0)
 
 
 if __name__ == '__main__':
